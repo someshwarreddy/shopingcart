702baadcad41e18b5484929c2e6a9262f92809ef
someshwar reddy <somesh.satish@gmail.com>
2023-08-02 16:50:55 +0530

    firstcommit

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnChanges, OnInit } from '@angular/core';
 import { SearchtermService } from '../datashare/searchterm.service';
 import { CartserviceService } from '../cartservice.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -10,7 +11,7 @@ export class MenuComponent implements AfterViewInit {
   searchTerm: string;
   public cartCount: number = 0;
 
-  constructor(private SearchtermService: SearchtermService, private cartService: CartserviceService) {
+  constructor(private SearchtermService: SearchtermService, private cartService: CartserviceService,   private router: Router,) {
 
   }
   ngAfterViewInit(): void {
@@ -27,5 +28,8 @@ export class MenuComponent implements AfterViewInit {
     console.log(this.searchTerm)
     this.SearchtermService.setSearchterm(this.searchTerm)
   }
-
+  goToCart() {
+    this.cartCount = 0;
+   this.router.navigate(['cart']);
+  }
 }
